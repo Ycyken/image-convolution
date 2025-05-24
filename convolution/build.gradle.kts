8e48b1eb11ff5c4f231adb7ab834e63c9e37c501
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2025-05-24 16:22:59 +0300

    feat: implement parallel convolution by rows, cols and elemens
    Signed-off-by: Gleb Nasretdinov <gleb.nasretdinov@proton.me>

--- convolution/build.gradle.kts
+++ convolution/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
     id("buildlogic.kotlin-library-conventions")
 }
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+}
--- convolution/src/main/kotlin/convolution/Convolution.kt
+++ convolution/src/main/kotlin/convolution/Convolution.kt
@@ -2,6 +2,10 @@ package convolution
 
 import boofcv.io.image.ConvertBufferedImage
 import boofcv.struct.image.GrayU8
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.awt.image.BufferedImage
 import kotlin.math.roundToInt
 
@@ -38,7 +42,10 @@ private fun convolveGray(
     kernel: Kernel,
 ): BufferedImage {
     val gray = ConvertBufferedImage.convertFromSingle(image, null, GrayU8::class.java)
-    val convolved = gray.convolve(kernel)
+    val convolved =
+        runBlocking {
+            gray.convolve(kernel)
+        }
     return ConvertBufferedImage.convertTo(convolved, null)
 }
 
@@ -54,45 +61,21 @@ private fun convolveRGB(
             GrayU8::class.java,
         )
     val convolved = planar.createSameShape()
-    planar.bands.forEachIndexed { i, band -> convolved.setBand(i, band.convolve(kernel)) }
+    runBlocking {
+        planar.bands.forEachIndexed { i, band ->
+            launch(Dispatchers.Default) {
+                convolved.setBand(
+                    i,
+                    band.convolve(kernel),
+                )
+            }
+        }
+    }
+
     return ConvertBufferedImage.convertTo_U8(convolved, null, true)
 }
 
-private fun <T : Number> convolve(
-    input: ReadableMatrix<T>,
-    kernel: Kernel,
-    output: WritableMatrix<T>,
-    transform: (Float) -> T,
-) {
-    require(kernel.width % 2 == 1 && kernel.width == kernel.height) { "Kernel must be square matrix with odd size" }
-    require(input.width == output.width && input.height == output.height) { "Input matrix must be same size as output matrix" }
-
-    val kernelCenter = kernel.width / 2
-    input.forEachIndexed { x, y, inputValue ->
-        var convolvedValue = 0.0F
-        var usedWeights = 0.0F
-        kernel.forEachIndexed { kernelX, kernelY, kernelValue ->
-            try {
-                val correspondingValue =
-                    input[
-                        x + (kernelX - kernelCenter),
-                        y + (kernelY - kernelCenter),
-                    ].toFloat()
-                convolvedValue += correspondingValue * kernelValue
-                usedWeights += kernelValue
-            } catch (_: Exception) {
-            }
-        }
-
-        if (usedWeights != 0.0F) {
-            convolvedValue /= usedWeights
-        }
-        val transformedValue = transform(convolvedValue)
-        output.unsafeSet(x, y, transformedValue)
-    }
-}
-
-internal fun GrayU8.convolve(kernel: Kernel): GrayU8 {
+internal suspend fun GrayU8.convolve(kernel: Kernel): GrayU8 {
     val input = MatrixAdapter(this)
     val output = MatrixAdapter(this.createSameShape())
     val transform = { x: Float -> x.roundToInt().coerceIn(0, 255) }
@@ -105,4 +88,106 @@ internal fun Kernel.convolve(other: Kernel): Kernel {
     val transform = { x: Float -> x.coerceIn(0.0F, 255.0F) }
     convolve(this, other, output, transform)
     return output
+}
+
+private fun <T : Number> convolvePoint(
+    input: ReadableMatrix<T>,
+    kernel: Kernel,
+    output: WritableMatrix<T>,
+    transform: (Float) -> T,
+    x: Int,
+    y: Int,
+) {
+    val kernelCenter = kernel.width / 2
+    var convolvedValue = 0.0F
+    var usedWeights = 0.0F
+    kernel.forEachIndexed { kernelX, kernelY, kernelValue ->
+        try {
+            val correspondingValue =
+                input[
+                    x + (kernelX - kernelCenter),
+                    y + (kernelY - kernelCenter),
+                ].toFloat()
+            convolvedValue += correspondingValue * kernelValue
+            usedWeights += kernelValue
+        } catch (_: Exception) {
+        }
+    }
+
+    if (usedWeights != 0.0F) {
+        convolvedValue /= usedWeights
+    }
+    val transformedValue = transform(convolvedValue)
+    output.unsafeSet(x, y, transformedValue)
+}
+
+private fun <T> validateConvolutionArgs(
+    input: ReadableMatrix<T>,
+    kernel: Kernel,
+    output: WritableMatrix<T>,
+) {
+    require(kernel.width % 2 == 1 && kernel.width == kernel.height) { "Kernel must be square matrix with odd size" }
+    require(input.width == output.width && input.height == output.height) { "Input matrix must be same size as output matrix" }
+}
+
+private fun <T : Number> convolve(
+    input: ReadableMatrix<T>,
+    kernel: Kernel,
+    output: WritableMatrix<T>,
+    transform: (Float) -> T,
+) {
+    validateConvolutionArgs(input, kernel, output)
+
+    input.forEachIndexed { x, y, _ ->
+        convolvePoint(input, kernel, output, transform, x, y)
+    }
+}
+
+private suspend fun <T : Number> convolveParRows(
+    input: ReadableMatrix<T>,
+    kernel: Kernel,
+    output: WritableMatrix<T>,
+    transform: (Float) -> T,
+) = coroutineScope {
+    validateConvolutionArgs(input, kernel, output)
+
+    for (y in 0 until input.height) {
+        launch(Dispatchers.Default) {
+            for (x in 0 until input.width) {
+                convolvePoint(input, kernel, output, transform, x, y)
+            }
+        }
+    }
+}
+
+private suspend fun <T : Number> convolveParCols(
+    input: ReadableMatrix<T>,
+    kernel: Kernel,
+    output: WritableMatrix<T>,
+    transform: (Float) -> T,
+) = coroutineScope {
+    validateConvolutionArgs(input, kernel, output)
+
+    for (x in 0 until input.width) {
+        launch(Dispatchers.Default) {
+            for (y in 0 until input.height) {
+                convolvePoint(input, kernel, output, transform, x, y)
+            }
+        }
+    }
+}
+
+private suspend fun <T : Number> convolveParElements(
+    input: ReadableMatrix<T>,
+    kernel: Kernel,
+    output: WritableMatrix<T>,
+    transform: (Float) -> T,
+) = coroutineScope {
+    validateConvolutionArgs(input, kernel, output)
+
+    input.forEachIndexed { x, y, _ ->
+        launch(Dispatchers.Default) {
+            convolvePoint(input, kernel, output, transform, x, y)
+        }
+    }
 }
