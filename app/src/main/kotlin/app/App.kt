b3140067c6b47f2ce586cba30b709854fd4c7c92
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2025-05-25 12:29:53 +0300

    feat: add image load and save time measurement
    Signed-off-by: Gleb Nasretdinov <gleb.nasretdinov@proton.me>

--- app/src/main/kotlin/app/App.kt
+++ app/src/main/kotlin/app/App.kt
@@ -3,7 +3,7 @@ package app
 import boofcv.io.image.UtilImageIO
 import convolution.ConvMode
 import convolution.convolve
-import kernels.motionBlur
+import kernels.boxBlur
 import java.io.File
 import javax.imageio.ImageIO
 
@@ -12,16 +12,22 @@ fun main(args: Array<String>) {
         println("Enter the file name")
         return
     }
+    val startLoad = System.currentTimeMillis()
     val projectDir = File(System.getProperty("rootProjectDir"))
     val sourcePath = projectDir.resolve(args[0]).absoluteFile.toString()
     val image = UtilImageIO.loadImage(sourcePath)!!
+    val endLoad = System.currentTimeMillis()
+    println("Load image took ${endLoad - startLoad} ms")
 
-    val kernel = motionBlur(9)
-    val start = System.currentTimeMillis()
-    val convolved = convolve(image, kernel, ConvMode.ParallelRows(1))
-    val end = System.currentTimeMillis()
-    println("Convolution took ${end - start} ms")
+    val startConv = System.currentTimeMillis()
+    val kernel = boxBlur(21)
+    val convolved = convolve(image, kernel, ConvMode.ParallelRows(5))
+    val endConv = System.currentTimeMillis()
+    println("Convolution took ${endConv - startConv} ms")
 
+    val startSave = System.currentTimeMillis()
     val outputFile = File(projectDir, "output_image")
     ImageIO.write(convolved, "bmp", outputFile)
+    val endSave = System.currentTimeMillis()
+    println("Convolution took ${endSave - startSave} ms")
 }
